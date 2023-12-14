b0c9d1e1f55714628bbadea53a4e56e47b5a9907
Nick <lolipop22234@gmail.com>
2023-12-14 17:28:14 +0200

    added gradle.yml

--- src/test/java/ru/netology/FormTest/FormTest.java
+++ src/test/java/ru/netology/FormTest/FormTest.java
@@ -1,9 +1,7 @@
 package ru.netology.FormTest;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
