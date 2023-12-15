bb2726fb6fe935ba57c04e38725907e15e56cdd0
Nick <lolipop22234@gmail.com>
2023-12-15 07:51:53 +0200

    Added dselenide.headless

--- src/test/java/ru/netology/FormTest/FormTest.java
+++ src/test/java/ru/netology/FormTest/FormTest.java
@@ -1,7 +1,6 @@
 package ru.netology.FormTest;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -10,7 +9,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class FormTest {
     @Test
     void shouldTest() {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Никита Курлат");
         $("[data-test-id=phone] input").setValue("+37376720458");
@@ -21,53 +19,48 @@ public class FormTest {
 
     @Test
     void shouldNotPassName() throws InterruptedException {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Ivan Ivanov");
         $("[data-test-id=phone] input").setValue("+37376720458");
         $("[data-test-id=agreement]").click();
         $("[role=button]").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotPassNoName() throws InterruptedException {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=phone] input").setValue("+37376720458");
         $("[data-test-id=agreement]").click();
         $("[role=button]").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotPassPhone() throws InterruptedException {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("876720458");
         $("[data-test-id=agreement]").click();
         $("[role=button]").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldNotPassNoPhone() throws InterruptedException {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=agreement]").click();
         $("[role=button]").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotPassCheckbox() {
-        Configuration.headless = true;
         open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("+37376720458");
         $("[role=button]").click();
-        $(".input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
